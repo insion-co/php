45aded5cdc4fd17ad1b7fc8a77621fcffde9e3f5
Mehdi El Oualy <mehdieloualy@gmail.com>
2026-07-31 14:42:39 +0100

    chore: prepare PHP SDK for Packagist

--- src/InsionClient.php
+++ src/InsionClient.php
@@ -63,6 +63,7 @@ class InsionClient
             'Authorization' => "Bearer $token",
             'X-Fern-Language' => 'PHP',
             'X-Fern-SDK-Name' => 'Insion',
+            'User-Agent' => 'insion/insion/1.0.1',
         ];
 
         $this->options = $options ?? [];
